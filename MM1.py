9535fc7b3a8c36d46037a660119aff3ab9baa823
Gianmarco Garrisi <gianmarcogarrisi@tutanota.com>
2018-04-11 10:43:06 +0200

    Refactoring: separated arrival-process and service-process classes in other files

--- MM1.py
+++ MM1.py
@@ -3,6 +3,7 @@ import numpy as np
 import simpy
 import random
 import matplotlib.pyplot as plt
+from netsimutils import *
 
 
 # **********************************************************************************************************************
@@ -15,74 +16,6 @@ NUM_MACHINES = 4
 QUEUE_SIZE = 50
 
 SIM_TIME = 1000
-
-# **********************************************************************************************************************
-# Packet arrival
-# **********************************************************************************************************************
-#
-class PacketArrival(object):
-    """ Questa classe genera soltanto l'arrivo dei processi
-"""
-    # constructor
-    def __init__(self, environ, arrival_time):
-
-        # the inter-arrival time
-        self.arrival_time = arrival_time
-
-        # the environment
-        self.env = environ
-
-        self.ia_times = []
-        # execute the process
-    def arrival_process(self, server_farm):
-        while True:
-            # sample the time to next arrival
-            inter_arrival = random.expovariate(lambd=1.0/self.arrival_time)
-            self.ia_times.append(inter_arrival)
-
-            # yield an event to the simulator
-            yield self.env.timeout(inter_arrival)
-
-            # a car has arrived - request carwash to do its job
-            self.env.process(server_farm.service())
-
-
-# **********************************************************************************************************************
-# Packet Processing - it gets a waiting process (FCFS) and performs the service
-# **********************************************************************************************************************
-class Service(object):
-    """Questo mette in coda o scarta i pacchetti e serve le richieste
-(Segue il pacchetto dall'inserimento in coda fino alla fine del servizio)
-
-    """
-    def __init__(self, environment, n_servers, s_time):
-        self.environment = environment
-        self.servers = simpy.Resource(environment, n_servers)
-        self.s_time = s_time
-        self.qsize = 0
-        self.lost = 0
-        self.re_times = []
-
-        
-    def service(self):
-        #print("Packets in queue: %d" % self.qsize)
-        if self.qsize == QUEUE_SIZE:
-            #print("Packet lost at %f" % env.now )
-            self.lost += 1
-        else:
-            self.qsize += 1
-            t0 = self.environment.now
-            #print("Packet arrived in queue at %f" % env.now)
-            with self.servers.request() as req:
-                yield req
-
-                service_time = random.expovariate(1.0/self.s_time)
-                yield self.environment.timeout(service_time)
-                #print("Packet left the server at %f" % env.now)
-                t1 = self.environment.now
-                self.re_times.append(t1-t0)
-                self.qsize -= 1
-
 
 def n_independent_queues():
     # ********************************
@@ -97,7 +30,7 @@ def n_independent_queues():
     for _ in range(NUM_MACHINES):
         env = simpy.Environment()
         packet_arrival = PacketArrival(env, NUM_MACHINES*INTER_ARRIVAL )
-        server_farm = Service(env, 1, SERVICE_TIME)
+        server_farm = Service(env, 1, SERVICE_TIME, QUEUE_SIZE)
         arrivals.append(packet_arrival)
         servers.append(server_farm)
 
@@ -174,7 +107,7 @@ def n_services():
     packet_arrival = PacketArrival(env, INTER_ARRIVAL)
 
     # carwash
-    server_farm = Service(env, NUM_MACHINES, SERVICE_TIME)
+    server_farm = Service(env, NUM_MACHINES, SERVICE_TIME, QUEUE_SIZE)
 
     # start the arrival process
     env.process(packet_arrival.arrival_process(server_farm))
@@ -219,7 +152,7 @@ def fast_service():
     packet_arrival = PacketArrival(env, INTER_ARRIVAL)
 
     # carwash
-    server_farm = Service(env, 1, SERVICE_TIME/NUM_MACHINES)
+    server_farm = Service(env, 1, SERVICE_TIME/NUM_MACHINES, QUEUE_SIZE)
 
     # start the arrival process
     env.process(packet_arrival.arrival_process(server_farm))
